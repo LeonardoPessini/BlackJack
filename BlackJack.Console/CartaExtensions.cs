1cd43f99e95ab16821d9ece97283c11dfc1c6f40
LeonardoPessini <leopessini.net@gmail.com>
2025-05-19 10:49:27 -0300

    Conversor de carta Model-View

---
+++ BlackJack.Console/CartaExtensions.cs
@@ -0,0 +1,39 @@
+﻿using BlackJack.Domain.Cartas;
+
+namespace BlackJack.Console;
+
+internal static class CartaExtensions
+{
+    internal static CartaView ToConsole(this Carta carta)
+    {
+        var valor = carta.Valor switch
+        {
+            ValorCarta.As => Simbolo.As,
+            ValorCarta.Dois => Simbolo.Dois,
+            ValorCarta.Tres => Simbolo.Tres,
+            ValorCarta.Quatro => Simbolo.Quatro,
+            ValorCarta.Cinco => Simbolo.Cinco,
+            ValorCarta.Seis => Simbolo.Seis,
+            ValorCarta.Sete => Simbolo.Sete,
+            ValorCarta.Oito => Simbolo.Oito,
+            ValorCarta.Nove => Simbolo.Nove,
+            ValorCarta.Dez => Simbolo.Dez,
+            ValorCarta.Valete => Simbolo.Valete,
+            ValorCarta.Dama => Simbolo.Dama,
+            ValorCarta.Rei => Simbolo.Rei,
+            _ => null
+        };
+
+        var naipe = carta.Naipe switch
+        {
+            Naipe.Paus => Simbolo.Paus,
+            Naipe.Copa => Simbolo.Copa,
+            Naipe.Ouro => Simbolo.Ouro,
+            Naipe.Espadas => Simbolo.Espadas,
+            _ => null
+        };
+
+        return new CartaView(valor, naipe);
+    }
+
+}
---
+++ BlackJack.Console/Simbolo.cs
@@ -0,0 +1,24 @@
+﻿
+namespace BlackJack.Console;
+
+internal static class Simbolo
+{
+    internal const string As = "A";
+    internal const string Rei = "K";
+    internal const string Dama = "Q";
+    internal const string Valete = "J";
+    internal const string Dez = "10";
+    internal const string Nove = "9";
+    internal const string Oito = "8";
+    internal const string Sete = "7";
+    internal const string Seis = "6";
+    internal const string Cinco = "5";
+    internal const string Quatro = "4";
+    internal const string Tres = "3";
+    internal const string Dois = "2";
+
+    internal const string Ouro = "♦";
+    internal const string Espadas = "♠";
+    internal const string Paus = "♣";
+    internal const string Copa = "♥";
+}
