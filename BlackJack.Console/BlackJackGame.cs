b1354faf09ee5c73b1e638f7e26cb247dacd49cb
LeonardoPessini <leopessini.net@gmail.com>
2025-05-19 10:52:43 -0300

    Adicionando inicializar do jogo O usuário e o valor da aposta são aramzenados em memória, pois ainda é um protótipo inicial

---
+++ BlackJack.Console/BlackJackGame.cs
@@ -0,0 +1,42 @@
+﻿using BlackJack.Domain;
+using System;
+
+namespace BlackJack.Console;
+
+internal class BlackJackGame
+{
+    private int _jogadorId;
+
+    public BlackJackGame()
+    {
+        _jogadorId = Janela.SolicitarId();        
+    }
+
+    internal void StartGame()
+    {
+        var valorDaAposta = Janela.SolicitarValorDaAposta();
+
+        var jogo = new Jogo(_jogadorId, valorDaAposta);
+        var janela = new Janela(jogo);
+        var input = new InputOpcao(jogo);
+
+        while (!jogo.PartidaFinalizada){
+            janela.Renderizar();
+
+            while (input.Entry()) ;
+        }
+
+        var resultado = jogo.ObterResultado();
+
+        janela.Renderizar();
+
+        if (resultado.Vencedor == Vencedor.Apostador)
+            janela.RedenrizarVitoria();
+
+        else if( resultado.Vencedor == Vencedor.Mesa)
+            janela.RedenrizarDerrota();
+
+        else 
+            janela.RedenrizarEmpate();
+    }
+}
---
+++ BlackJack.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using BlackJack.Console;
+using BlackJack.Domain;
+
+var game = new BlackJackGame();
+
+bool jogarNovamente;
+
+do
+{
+    try
+    {
+        game.StartGame();
+
+        jogarNovamente = Janela.DesejaJogarNovamente();
+    }catch(ExitException)
+    {
+        jogarNovamente = false;
+    }
+    
+}while(jogarNovamente);
