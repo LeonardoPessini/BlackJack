f246734d3c864b92ca11310ff74a0c93b553649d
LeonardoPessini <leopessini.net@gmail.com>
2025-05-19 10:23:04 -0300

    Add temporary mock Usuario

---
+++ BlackJack.Domain/Apostadores/UsuarioUserCase.cs
@@ -0,0 +1,26 @@
+﻿namespace BlackJack.Domain.Apostadores;
+
+internal class UsuarioUseCase
+{
+    private static List<Usuario> _usuarios;
+
+    static UsuarioUseCase()
+    {
+        _usuarios = new List<Usuario>();
+    }
+
+    internal void Add(Usuario usuario)
+    {
+        _usuarios.Add(usuario);
+    }
+
+    internal Usuario? Get(int usuarioId)
+    {
+        return _usuarios.First(u => u.Id == usuarioId);
+    }
+
+    internal void Delete(Usuario usuario)
+    {
+        _usuarios.Remove(usuario);
+    }
+}
