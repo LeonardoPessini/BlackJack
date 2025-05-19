2b4156ea33790e4f4d9f9a98255ec0ea52d182b4
LeonardoPessini <leopessini.net@gmail.com>
2025-05-19 10:22:17 -0300

    Add Domain Usuario

---
+++ BlackJack.Domain/Apostadores/Usuario.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+
+namespace BlackJack.Domain.Apostadores;
+
+internal class Usuario
+{
+    internal int Id { get; set; }
+    internal decimal Saldo { get; private set; }
+
+    internal Usuario(int id, decimal saldo)
+    {
+        if (id <= 0)
+            throw new ArgumentException("Id invalido");
+
+        if (saldo < 1)
+            throw new ArgumentException("Saldo invalido");
+
+        Id = id;
+        Saldo = saldo;
+    }
+
+    internal void RetirarSaldo(decimal valor)
+    {
+        if(valor <= 0) 
+            throw new ArgumentException("Valor invalido");
+
+        if (valor > Saldo)
+            throw new InvalidOperationException("Saldo insulficiente");
+
+        Saldo -= valor;
+    }
+
+    internal void AdicionarSaldo(decimal valor)
+    {
+        if (valor <= 0)
+            throw new ArgumentException("Valor invalido");
+
+        Saldo += valor;
+    }
+}
---
+++ BlackJack.Test/UsuarioTest.cs
@@ -0,0 +1,79 @@
+﻿using BlackJack.Domain.Apostadores;
+
+namespace BlackJack.Test;
+
+public class UsuarioTest
+{
+    private Usuario _usuario;
+    public UsuarioTest()
+    {
+        _usuario = new Usuario(id: 1, saldo: 100);
+    }
+
+    [Fact]
+    public void DeveCriarUsuario()
+    {
+        Assert.Equal(1, _usuario.Id);
+        Assert.Equal(100, _usuario.Saldo);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void NaoDeveCriarUsuarioComIdInvalido(int idInvalido)
+    {
+        var message = Assert.Throws<ArgumentException>(() => new Usuario(id:idInvalido, saldo: 100)).Message;
+        Assert.Equal("Id invalido", message);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void NaoDeveCriarUsuarioComSaldoInvalido(int saldoInvalido)
+    {
+        var message = Assert.Throws<ArgumentException>(() => new Usuario(id: 1, saldo: saldoInvalido)).Message;
+        Assert.Equal("Saldo invalido", message);
+    }
+
+
+    [Theory]
+    [InlineData(1, 101)]
+    public void DeveAtualizarSaldoAposInserir(decimal saldoAInserir, decimal saldoFinal)
+    {
+        _usuario.AdicionarSaldo(saldoAInserir);
+        Assert.Equal(saldoFinal, _usuario.Saldo);
+    }
+
+    [Theory]
+    [InlineData(1, 99)]
+    public void DeveAtualizarSaldoAposRemover(decimal saldoARemover, decimal saldoFinal)
+    {
+        _usuario.RetirarSaldo(saldoARemover);
+        Assert.Equal(saldoFinal, _usuario.Saldo);
+    }
+
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void NaoDeveInserirZeroOuNegativo(decimal saldoAInserir)
+    {
+        Assert.Throws<ArgumentException>(() => _usuario.AdicionarSaldo(saldoAInserir));
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void NaoDeveRemoverZeroOuNegativo(decimal saldoAInserir)
+    {
+        Assert.Throws<ArgumentException>(() => _usuario.RetirarSaldo(saldoAInserir));
+    }
+
+
+    [Fact]
+    public void NaoDeveRetirarSaldoSeSaldoInsuficiente()
+    {
+        var message = Assert.Throws<InvalidOperationException>(() => _usuario.RetirarSaldo(101)).Message;
+        Assert.Equal("Saldo insulficiente", message);
+    }
+}
